8f5800c430a72e7ecd06b62f0a96da50e0e9d85e
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-02 22:16:28 -0300

    Acho que ta certo

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/script5.js
@@ -0,0 +1,10 @@
+const a1 = 80;
+const a2 = 50;
+const a3 = 49;
+let soma = a1 + a2 + a3;
+
+if (soma === 180) {
+    console.log(true);
+} else {
+    console.log(false, "\nAlgum dos ângulos excede ou não atinge os requerimentos necessários para formarem um triângulo válido");
+}
