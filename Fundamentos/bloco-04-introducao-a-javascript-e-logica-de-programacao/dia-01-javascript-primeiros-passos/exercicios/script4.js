43ec71c520eff5b04acffeebb216327a5d06571a
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-02 22:07:32 -0300

    Adiciona exercicio 4

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/script4.js
@@ -0,0 +1,27 @@
+const num = 0;
+
+if (num > 0) {
+    console.log("positive");
+} else if (num < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
+
+// Com switch
+
+/*
+const num = 0;
+
+switch (true) {
+    case (num > 0):
+        console.log("positive");
+        break;
+    case (num < 0):
+        console.log("negative");
+        break;
+    default:
+        console.log("zero");
+        break;
+}
+*/
