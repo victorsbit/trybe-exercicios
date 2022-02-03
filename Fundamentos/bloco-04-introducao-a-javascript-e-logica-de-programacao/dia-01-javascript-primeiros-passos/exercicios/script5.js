cd6af01a1b47a8ee2b6a596e5882b1c02d56c1d4
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-02 22:22:59 -0300

    Atualizando exercicio 5 (agora ta certo... eu acho)

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/script5.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/script5.js
@@ -1,10 +1,12 @@
 const a1 = 80;
 const a2 = 50;
-const a3 = 49;
+const a3 = 50;
 let soma = a1 + a2 + a3;
 
 if (soma === 180) {
     console.log(true);
+} else if (a1 <= 0 || a2 <= 0 || a3 <= 0) {
+    console.log("Um dos ângulos inseridos possui um valor inválido.");
 } else {
-    console.log(false, "\nAlgum dos ângulos excede ou não atinge os requerimentos necessários para formarem um triângulo válido");
+    console.log(false);
 }
