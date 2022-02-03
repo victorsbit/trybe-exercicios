bf136e518a126a878f30d032be8889bdbaa5518f
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-03 19:12:44 -0300

    for.js updated

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-js-array-e-loop-for/for.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-js-array-e-loop-for/for.js
@@ -3,3 +3,11 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (index = 0; index < groceryList.length; index += 1) {
     console.log(groceryList[index]);
 }
+
+// For Of
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let elements of names) {
+    console.log(elements);
+}
