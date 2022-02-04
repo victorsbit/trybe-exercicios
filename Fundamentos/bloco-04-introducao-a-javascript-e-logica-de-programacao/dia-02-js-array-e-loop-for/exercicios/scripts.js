7f3e88646046431eccd58460915a740f36292997
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-03 22:15:55 -0300

    Exercicio 7 adicionado

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-js-array-e-loop-for/exercicios/scripts.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-js-array-e-loop-for/exercicios/scripts.js
@@ -4,80 +4,80 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // com For
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     console.log(numbers[index]);
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}
 
 // com For Of
 
-// for (let index of numbers) {
-//     console.log(index);
-// }
+for (let index of numbers) {
+    console.log(index);
+}
 
 // Exercício 2
 
-// let soma = 0;
+let soma = 0;
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     soma += numbers[index];
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
 
-// console.log(soma);
+console.log(soma);
 
 // Exercício 3
 
-// let total = 0;
-// let media = 0;
+let total = 0;
+let media = 0;
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     total += numbers[index];
-//     media = total / numbers.length;
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    total += numbers[index];
+    media = total / numbers.length;
+}
 
-// console.log(media);
+console.log(media);
 
 // Exercício 4
 
-// let total = 0;
-// let media = 0;
+let total = 0;
+let media = 0;
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     total += numbers[index];
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    total += numbers[index];
+}
 
-// media = total / numbers.length;
+media = total / numbers.length;
 
-// if (media > 20) {
-//     console.log('valor maior que 20');
-// } else if (media < 20) {
-//     console.log('valor menor que 20');
-// }
+if (media > 20) {
+    console.log('valor maior que 20');
+} else if (media < 20) {
+    console.log('valor menor que 20');
+}
 
 // Exercício 5 | Referência: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 
-// let max = 0;
+let max = 0;
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     max = Math.max(...numbers);
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    max = Math.max(...numbers);
+}
 
-// console.log(max);
+console.log(max);
 
 // Exercício 6
 
-// let impar = 0;
-// let contadorImpar = 0;
+let impar = 0;
+let contadorImpar = 0;
 
-// for (index = 0; index < numbers.length; index += 1) {
-//     impar = numbers[index] % 2;
-//     if (impar != 0) {
-//         contadorImpar += 1;
-//     } else if (contadorImpar === 0) {
-//         console.log('Nenhum valor ímpar encontrado');
-//     }
-// }
+for (index = 0; index < numbers.length; index += 1) {
+    impar = numbers[index] % 2;
+    if (impar != 0) {
+        contadorImpar += 1;
+    } else if (contadorImpar === 0) {
+        console.log('Nenhum valor ímpar encontrado');
+    }
+}
 
-// console.log(contadorImpar);
+console.log(contadorImpar);
 
 // Exercício 7
 
