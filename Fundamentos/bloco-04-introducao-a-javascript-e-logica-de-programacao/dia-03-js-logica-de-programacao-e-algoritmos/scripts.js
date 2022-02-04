3e1d3f84a0130eb86201fd9a85d2a36e2ea8a702
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-04 20:50:45 -0300

    Atualizando scripts.js

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-js-logica-de-programacao-e-algoritmos/scripts.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-js-logica-de-programacao-e-algoritmos/scripts.js
@@ -11,7 +11,6 @@ let word = 'tryber';
 let wordReverse = '';
 
 for (let index = word.length - 1; index >= 0; index -= 1) {
-    // let wordReverse = word.split('').reverse().join('');
     wordReverse += word[index];
 }
 
