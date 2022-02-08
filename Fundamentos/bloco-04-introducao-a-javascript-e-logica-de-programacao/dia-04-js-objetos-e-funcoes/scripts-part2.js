1cae48106f4c30253b0aa93ed685d9da78fbd659
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-07 21:12:49 -0300

    Adiciona exercicio 1

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-js-objetos-e-funcoes/scripts-part2.js
@@ -0,0 +1,17 @@
+// Exercício 1 | Referència: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse and https://stackabuse.com/how-to-reverse-a-string-in-javascript/
+
+function verificaPalindromo(palindromo) {
+  
+  let string = palindromo; // Determino o valor da minha string
+  let splitString = string.split(''); // 'Quebro' a minha string em uma Array de diversas strings e atribuo o seu valor a variavel 'splitString'
+  let reverseArray = splitString.reverse(); // Inverto a ordem dos elementos da minha Array e atribuo à variavel 'reverseArray'
+  let reverseString = reverseArray.join(''); // 'Junto' a Array invertida novamente numa string e atribuo o seu valor a variavel 'reverseString'
+
+  if (string === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(verificaPalindromo('banana'));
