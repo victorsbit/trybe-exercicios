021572567f39dddf21422076cbba3366070f7d87
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-02-10 13:45:22 -0300

    Adiciona exercicio fixacao 4

--- Fundamentos/bloco-05-js-dom-eventos-web-storage/dia-02-js-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-05-js-dom-eventos-web-storage/dia-02-js-trabalhando-com-elementos/script.js
@@ -3,11 +3,17 @@ console.log(document.getElementById('start').nextSibling) // nó
 console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
 */
 
+// 1
 console.log(document.getElementById('primeiroFilhoDoFilho').parentNode);
 
+// 2
 let corPai = document.getElementById('elementoOndeVoceEsta').parentNode;
 corPai.style.color = 'violet';
 
+// 3
 let textPrimeiroFilhoDoFilho = document.getElementById('elementoOndeVoceEsta').firstElementChild;
 
 textPrimeiroFilhoDoFilho.innerText = 'muda!';
+
+// 4
+console.log(document.getElementById('pai').firstElementChild);
