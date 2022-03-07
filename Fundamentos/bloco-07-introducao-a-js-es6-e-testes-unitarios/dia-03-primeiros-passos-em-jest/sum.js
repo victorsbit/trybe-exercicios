c654405cc95138046f636c55a00f2d7fb65660ae
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-03-07 20:13:09 -0300

    Adiciona exercicio 1

--- Fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.js
+++ Fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.js
@@ -2,9 +2,7 @@ function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
   }
-
   return a + b;
 }
 
-console.log(sum(10, 2));
 module.exports = sum;
