1a5dc4a8d5ec92b73015988b3f314109dc527db9
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-03-14 18:29:34 -0300

    Adiciona exercicio 1

--- Fundamentos/bloco-08-higher-order-functions-js-es6/dia-04-js-es6-hof-reduce/script-fixacao.js
+++ Fundamentos/bloco-08-higher-order-functions-js-es6/dia-04-js-es6-hof-reduce/script-fixacao.js
@@ -1,7 +1,83 @@
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
-const evenNumbers = numbers.filter(element => element % 2 === 0).reduce((acc, index) => {
-  console.log(`${acc} + ${index}`);
-  return acc + index;
+const evenNumbers = numbers.filter(element => element % 2 === 0).reduce((acc, curr) => {
+  console.log(`${acc} + ${curr}`);
+  return acc + curr;
 });
 
-console.log(evenNumbers);
+// console.log(evenNumbers);
+
+// por fazer
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
---
+++ Fundamentos/bloco-08-higher-order-functions-js-es6/dia-04-js-es6-hof-reduce/script.js
@@ -0,0 +1,15 @@
+// 1
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acc, curr) => {
+    return acc.concat(curr);
+  }, []);
+}
+
+console.log(flatten());
