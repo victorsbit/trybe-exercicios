133c62a84c869e828292d048007ee9297b0ab6d7
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-03-15 20:35:11 -0300

    Altera exercicio 5

--- Fundamentos/bloco-08-higher-order-functions-js-es6/dia-05-js-es6-spread-operator-parametro-rest-destructuring-etc/exercise5.js
+++ Fundamentos/bloco-08-higher-order-functions-js-es6/dia-05-js-es6-spread-operator-parametro-rest-destructuring-etc/exercise5.js
@@ -2,7 +2,7 @@ const myList = [1, 2, 3];
 
 // escreva swap abaixo
 function swap(array) {
-  return [array[0], array[1], array[2]] = [array[2], array[1], array[0]];
+  return [a, b, c] = [array[2], array[1], array[0]];
 }
 
 console.log(swap(myList));
