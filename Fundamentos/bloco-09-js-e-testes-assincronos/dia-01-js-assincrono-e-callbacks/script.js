1e887759fd5c72f32de2e8b0c9ed21eee0d33fff
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-03-22 12:17:18 -0300

    Adiciona exercicio 4

--- Fundamentos/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/script.js
+++ Fundamentos/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/script.js
@@ -17,3 +17,22 @@ const getPlanet = () => {
 };
 
 setTimeout(getPlanet, 4000); // imprime Marte depois de 4 segundos
+
+// 4
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// console.log(getMarsTemperature());
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  return setTimeout(() => {
+    console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+  }, messageDelay());
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
