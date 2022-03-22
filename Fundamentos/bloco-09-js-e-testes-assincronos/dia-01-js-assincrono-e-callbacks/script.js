a289075f7cbb67b984bef20a727211ebe1929547
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-03-22 12:36:39 -0300

    Adiciona exercicio 6

--- Fundamentos/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/script.js
+++ Fundamentos/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/script.js
@@ -39,6 +39,7 @@ const sendMarsTemperature = () => {
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 */
 
+/* 
 // 5
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
@@ -64,3 +65,42 @@ const sendMarsTemperature = (callback) => {
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+*/
+
+// 6
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback, error) => {
+  return setTimeout(() => {
+    const randomNumber = Math.random();
+
+    if (randomNumber >= 0.4) {
+      callback((getMarsTemperature()));
+    } else {
+      error('y r dumb');
+    }
+  }, messageDelay());
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
