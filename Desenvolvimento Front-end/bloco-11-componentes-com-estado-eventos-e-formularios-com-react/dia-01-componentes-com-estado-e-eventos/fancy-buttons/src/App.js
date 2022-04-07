c230ab82a598243d4a5b8e70d552a53cd6c26ecf
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-04-07 18:42:53 -0300

    adiciona exercicio 4 de fixacao

--- Desenvolvimento Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Desenvolvimento Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,16 +3,27 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.logIt = this.logIt.bind(this);
+    this.logItt = this.logItt.bind(this);
+    this.logIttt = this.logIttt.bind(this);
+  }
+
   logIt() {
     console.log('clicou!');
+    console.log(this);
   }
   
   logItt() {
     console.log('clicou!!');
+    console.log(this);
   }
   
   logIttt() {
     console.log('clicou!!!');
+    console.log(this);
   }
 
   render() {
