f3c1084c251c494f629cbd3c0747557dd21a9627
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-04-07 18:40:17 -0300

    adiciona exercicio 3 de fixacao

--- Desenvolvimento Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Desenvolvimento Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
 import React, { Component } from 'react';
 
-function logIt() {
-  console.log('clicou!');
-}
-
-function logItt() {
-  console.log('clicou!!');
-}
-
-function logIttt() {
-  console.log('clicou!!!');
-}
 
 class App extends Component {
+  logIt() {
+    console.log('clicou!');
+  }
+  
+  logItt() {
+    console.log('clicou!!');
+  }
+  
+  logIttt() {
+    console.log('clicou!!!');
+  }
+
   render() {
     return (
       <main>
-        <button onClick={logIt}>Click!</button>
-        <button onClick={logItt}>Click!!</button>
-        <button onClick={logIttt}>Click!!!</button>
+        <button onClick={this.logIt}>Click!</button>
+        <button onClick={this.logItt}>Click!!</button>
+        <button onClick={this.logIttt}>Click!!!</button>
       </main>
     )
   }
