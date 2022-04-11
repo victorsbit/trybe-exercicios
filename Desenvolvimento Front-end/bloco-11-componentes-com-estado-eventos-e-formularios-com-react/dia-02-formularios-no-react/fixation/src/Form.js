b00e2280c1329907d2eae7b15c5f2e03c9f50e7e
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-04-11 14:36:56 -0300

    getting on track

---
+++ Desenvolvimento Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/fixation/src/Form.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+// import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = { 
+      textao: 'hello',
+      selecao: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({ 
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor='selecao'>Selecione a sua seleciocidade</label>
+          <br />
+          <select name='selecao' id='selecao' value={this.state.selecao} onChange={this.handleChange}>
+            <option value="" selected disabled hidden>escolha aqui</option>
+            <option value="selecao 1">selecao 1</option>
+            <option value="selecao 2">selecao 2</option>
+          </select>
+          <br />
+          <label htmlFor='textao'>Digite aqui</label>
+          <br />
+          <textarea id='textao' name='textao' value={this.state.textao} onChange={this.handleChange}></textarea>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form;
