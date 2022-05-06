02d19543c6010097e6619b2f20403231f36e0f7c
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-05-06 16:01:34 -0300

    exercicio 4

---
+++ Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.js
@@ -0,0 +1,13 @@
+function toUpperCase(str) {
+  return str.toUpperCase();
+}
+
+function firstLetter(str) {
+  return str[0];
+}
+
+function strConcat(a, b) {
+  return `${a}${b}`;
+}
+
+module.exports = { toUpperCase, firstLetter, strConcat };
--- Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
+++ Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
@@ -1,6 +1,20 @@
+const service = require('./service');
+
 function randomNumber() {
   return Math.floor(Math.random() * 100);
 }
+
+// function toUpperCase(str) {
+//   return str.toUpperCase();
+// }
+
+// function firstLetter(str) {
+//   return str[0];
+// }
+
+// function strConcat(a, b) {
+//   return `${a} ${b}`;
+// }
 
 describe('Exercises', () => {
   it('1', () => {
@@ -43,5 +57,31 @@ describe('Exercises', () => {
     expect(randomNumber).toHaveBeenCalled();
     expect(randomNumber).toHaveBeenCalledTimes(1);
     expect(randomNumber(5)).toBe(10);
+  })
+
+  it('4', () => {
+    jest.spyOn(service, 'toUpperCase').mockImplementation((str) => str.toLowerCase());
+    jest.spyOn(service, 'firstLetter').mockImplementation((str) => str[str.length - 1]);
+    jest.spyOn(service, 'strConcat').mockImplementation((a, b, c) => `${a}${b}${c}`);
+
+    service.toUpperCase('TOLOWERCASE');
+    expect(service.toUpperCase).toHaveBeenCalled();
+    expect(service.toUpperCase).toHaveBeenCalledTimes(1);
+    expect(service.toUpperCase('TOLOWERCASE')).toBe('tolowercase');
+
+    service.firstLetter('Last');
+    expect(service.firstLetter).toHaveBeenCalled();
+    expect(service.firstLetter).toHaveBeenCalledTimes(1);
+    expect(service.firstLetter('Last')).toBe('t');
+
+    service.strConcat('a', 'b', 'c');
+    expect(service.strConcat).toHaveBeenCalled();
+    expect(service.strConcat).toHaveBeenCalledTimes(1);
+    expect(service.strConcat('a', 'b', 'c')).toBe('abc');
+    
+    service.strConcat.mockRestore();
+    // expect(service.strConcat).toHaveBeenCalled(); // Como a implementação original da função foi restaurada (linha 82), não é mais possível executar esse matcher
+    // expect(service.strConcat).toHaveBeenCalledTimes(1); // Como a implementação original da função foi restaurada (linha 82), não é mais possível executar esse matcher
+    expect(service.strConcat('a', 'b')).toBe('ab');
   })
 });
