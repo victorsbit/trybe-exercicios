09c84d3a85abd6c911fca629c07c7ce6000406f6
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-05-05 21:39:54 -0300

    exercicio 1

--- Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
+++ Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
@@ -1,1 +1,17 @@
-// initial commit
+function randomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+describe('Exercises', () => {
+  it('1', () => {
+    const expected = 'number';
+    randomNumber();
+    expect(typeof randomNumber()).toBe(expected);
+    
+    randomNumber = jest.fn().mockReturnValue(10);
+    randomNumber();
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber()).toBe(10);
+  });
+});
