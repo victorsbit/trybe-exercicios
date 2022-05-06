f8ab401617315337f462f5d1f384c4db47a2d314
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-05-06 15:01:29 -0300

    exercicio 3

--- Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
+++ Desenvolvimento Front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exercicios/mocks-e-inputs/src/service/service.test.js
@@ -7,20 +7,41 @@ describe('Exercises', () => {
     const expected = 'number';
     randomNumber();
     expect(typeof randomNumber()).toBe(expected);
-    
+
     randomNumber = jest.fn().mockReturnValue(10);
     randomNumber();
     expect(randomNumber).toHaveBeenCalled();
     expect(randomNumber).toHaveBeenCalledTimes(1);
     expect(randomNumber()).toBe(10);
+
+    randomNumber.mockClear();
   });
 
   it('2', () => {
-    const mockFunction = randomNumber.mockImplementation((a, b) => a / b);
-    mockFunction(4, 2);
+    randomNumber.mockImplementation((a, b) => a / b);
+    randomNumber(4, 2);
 
-    expect(mockFunction).toHaveBeenCalled();
-    expect(mockFunction).toHaveBeenCalledWith(4, 2);
-    expect(mockFunction(4, 2)).toBe(2);
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber).toHaveBeenCalledWith(4, 2);
+    expect(randomNumber(4, 2)).toBe(2)
+    randomNumber.mockReset();
   });
+
+  it('3', () => {
+    randomNumber.mockImplementation((a, b, c) => a * b * c);
+    randomNumber(2, 2, 2);
+
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber(2, 2, 2)).toBe(8);
+    randomNumber.mockClear();
+
+    randomNumber.mockImplementation((a) => a * 2);
+    randomNumber(10);
+
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber(5)).toBe(10);
+  })
 });
