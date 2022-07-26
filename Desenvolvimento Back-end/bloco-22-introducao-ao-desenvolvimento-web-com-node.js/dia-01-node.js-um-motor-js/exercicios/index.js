ed4329b2ba8822fd26e78aff63cf41bf4db6deb5
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-07-26 19:14:15 -0300

    add exercicio 5

--- Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-01-node.js-um-motor-js/exercicios/index.js
+++ Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-01-node.js-um-motor-js/exercicios/index.js
@@ -4,4 +4,21 @@ const rs = require('readline-sync');
 const weight = rs.questionFloat('What is your weight? ');
 const height = rs.questionFloat('What is your height? ');
 
-console.log(calcBMI(weight, height));
+const result = calcBMI(weight, height).toFixed(2);
+
+switch (true) {
+  case result < 18.5:
+    console.log(`Your BMI of ${result} is considered underweight`);
+    break;
+  case result >= 18.5 && result <= 24.9:
+    console.log(`Your BMI of ${result} is considered normal weight`);
+    break;
+  case result >= 25 && result <= 29.9:
+    console.log(`Your BMI of ${result} is considered overweight`);
+    break;
+  case result >= 30:
+    console.log('You are tremendously fat');
+    break;
+  default:
+    console.log('dunno');
+}
