473853c7fece81c9a6414c380b49be31e156f2b6
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-07-28 16:00:25 -0300

    add exercicio 2

--- Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-fluxo-assincrono/exercicios/exercicio_1.js
+++ Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-02-node.js-fluxo-assincrono/exercicios/exercicio_1.js
@@ -16,13 +16,29 @@ function calc(a, b, c) {
   });
 }
 
-async function main() {
-  try {
-    const resultado = await calc(2, 2, 5);
-    console.log(`Resultado: ${resultado}`);
-  } catch(e) {
-    console.log(e.message);
-  }
+// async function main() {
+//   try {
+//     const resultado = await calc('a', 2, 5);
+//     console.log(`Resultado: ${resultado}`);
+//   } catch(e) {
+//     console.log(e.message);
+//   }
+// }
+
+// main();
+
+function doCalc() {
+  const rNumber1 = Math.floor(Math.random() * 100 + 1);
+  const rNumber2 = Math.floor(Math.random() * 100 + 1);
+  const rNumber3 = Math.floor(Math.random() * 100 + 1);
+
+  calc(rNumber1, rNumber2, 'number')
+    .then((resolve) => console.log(resolve))
+    .catch((error) => console.log(error.message));
+
+  calc(rNumber1, rNumber2, rNumber3)
+    .then((resolve) => console.log(resolve))
+    .catch((error) => console.log(error.message));
 }
 
-main();
+doCalc();
