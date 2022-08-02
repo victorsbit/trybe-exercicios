caaeabaafd95a145692972d760468c7ab514964e
Victor Stettner <victor.s.bitencourt@gmail.com>
2022-08-01 22:50:35 -0300

    2

--- Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-04-express-http-com-node.js/exercicios/index.js
+++ Desenvolvimento Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node.js/dia-04-express-http-com-node.js/exercicios/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 const PORT = 3000;
 
+// 1
 app.get('/ping', function (req, res) {
   res.status(200).json({ message: 'pong' });
+});
+
+// 2
+app.post('/hello', function (req, res) {
+  const { name } = req.body;
+
+  res.status(201).json({ message: `Hello, ${name}` });
 });
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
