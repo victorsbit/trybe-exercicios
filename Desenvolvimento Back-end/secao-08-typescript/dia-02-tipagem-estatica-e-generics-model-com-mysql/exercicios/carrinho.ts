ddd5c6fedf6c39531e550bb04a1f97c3dacc7486
Victor S <victor.s.bitencourt@gmail.com>
2022-09-12 13:27:18 -0300

    3, 4 e 5

---
+++ Desenvolvimento Back-end/secao-08-typescript/dia-02-tipagem-estatica-e-generics-model-com-mysql/exercicios/carrinho.ts
@@ -0,0 +1,75 @@
+enum Colors {
+    BLACK = 'Preta',
+    WHITE = 'Branca',
+    RED = 'Vermelha',
+    SILVER = 'Prata'
+  }
+  
+  enum Door {
+    OPEN = 'abre a porta',
+    CLOSE = 'fecha a porta'
+  }
+  
+  enum Direction {
+    LEFT = 'esquerda',
+    RIGHT = 'direita',
+  }
+  
+  class Car {
+    brand: string;
+    doors: number;
+    colors: Colors;
+  
+    constructor(brand: string, colors: Colors, doors: number) {
+      this.brand = brand;
+      this.doors = doors;
+      this.colors = colors;
+    }
+  
+    honk(): void {
+      console.log('why r u gae');
+    }
+  
+    openTheDoor(action: Door): void {
+      console.log(action);
+    }
+  
+    closeTheDoor(action: Door): void {
+      console.log(action);
+    }
+  
+    turnOn(): void {
+      console.log('ligando o carro');
+    }
+  
+    turnOff(): void {
+      console.log('desligando o carro');
+    }
+  
+    speedUp(): void {
+      console.log('acelerando');
+    }
+  
+    speedDown(): void {
+      console.log('desacelerando');
+    }
+  
+    stop(): void {
+      console.log('parou');
+    }
+  
+    turn(direction: Direction): void {
+      console.log(`virando a ${direction}`);
+    }
+  }
+  
+  const carrinho = new Car('Hotwheels', Colors.RED, 2);
+  
+  carrinho.turnOn();
+  carrinho.speedUp();
+  carrinho.turn(Direction.LEFT);
+  carrinho.turn(Direction.RIGHT);
+  carrinho.speedUp();
+  carrinho.turn(Direction.RIGHT);
+  carrinho.speedUp();
+  carrinho.turnOff();
--- Desenvolvimento Back-end/secao-08-typescript/dia-02-tipagem-estatica-e-generics-model-com-mysql/exercicios/index.ts
+++ Desenvolvimento Back-end/secao-08-typescript/dia-02-tipagem-estatica-e-generics-model-com-mysql/exercicios/index.ts
@@ -1,75 +1,28 @@
-enum Colors {
-  BLACK = 'Preta',
-  WHITE = 'Branca',
-  RED = 'Vermelha',
-  SILVER = 'Prata'
+type Slices = 4 | 6 | 8;
+
+interface Pizza {
+  flavor: string;
+  slices: Slices;
 }
 
-enum Door {
-  OPEN = 'abre a porta',
-  CLOSE = 'fecha a porta'
+const pizza1: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8,
 }
 
-enum Direction {
-  LEFT = 'esquerda',
-  RIGHT = 'direita',
+//
+
+type Comum = 'Calabresa' | 'Frango' | 'Pepperoni';
+
+type Vegetariana = 'Marguerita' | 'Palmito' | 'Cogumelo';
+
+type Doce = 'Nutela' | 'Goiabada com Queijo' | 'Marshmallow';
+
+interface PizzaComum extends Pizza {
+  flavor: Comum;
 }
 
-class Car {
-  brand: string;
-  doors: number;
-  colors: Colors;
-
-  constructor(brand: string, colors: Colors, doors: number) {
-    this.brand = brand;
-    this.doors = doors;
-    this.colors = colors;
-  }
-
-  honk(): void {
-    console.log('why r u gae');
-  }
-
-  openTheDoor(action: Door): void {
-    console.log(action);
-  }
-
-  closeTheDoor(action: Door): void {
-    console.log(action);
-  }
-
-  turnOn(): void {
-    console.log('ligando o carro');
-  }
-
-  turnOff(): void {
-    console.log('desligando o carro');
-  }
-
-  speedUp(): void {
-    console.log('acelerando');
-  }
-
-  speedDown(): void {
-    console.log('desacelerando');
-  }
-
-  stop(): void {
-    console.log('parou');
-  }
-
-  turn(direction: Direction): void {
-    console.log(`virando a ${direction}`);
-  }
+const umaPizzaComum: PizzaComum = {
+  flavor: 'Frango',
+  slices: 6,
 }
-
-const carrinho = new Car('Hotwheels', Colors.RED, 2);
-
-carrinho.turnOn();
-carrinho.speedUp();
-carrinho.turn(Direction.LEFT);
-carrinho.turn(Direction.RIGHT);
-carrinho.speedUp();
-carrinho.turn(Direction.RIGHT);
-carrinho.speedUp();
-carrinho.turnOff();
